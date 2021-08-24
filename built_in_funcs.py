4c254f63d872210b3b97ed295b7bc90e85142c86
sajicode <sajioloye@gmail.com>
2021-08-24 13:00:23 +0100

    chore: round up in built errors

--- built_in_funcs.py
+++ built_in_funcs.py
@@ -50,9 +50,9 @@ def remove_negatives(nums):
 people = ['Ted', 'Marshall', 'Lily', 'Barney', 'Robin']
 
 # returns true if all match the condition 
-print(all(name[0] == 'B' for name in people))
+# print(all(name[0] == 'B' for name in people))
 # returns true if at least one matches the condition
-print(any(name[0] == 'B' for name in people))
+# print(any(name[0] == 'B' for name in people))
 
 def is_all_strings(letters):
     return all(type(l) == str for l in letters)
@@ -66,21 +66,21 @@ num_list = [4,6,1,30,55,23]
 # print(sorted(num_list, reverse=True))
 
 # sort alphabetically by username
-print(sorted(users, key=lambda user: user['username']))
+# print(sorted(users, key=lambda user: user['username']))
 
 # sort by length of tweets
-print(sorted(users, key=lambda user: len(user['tweets']), reverse=True))
+# print(sorted(users, key=lambda user: len(user['tweets']), reverse=True))
 
 stark_names = ['Arya', 'Sansa', 'Brandon', 'Rob']
 
 # print min length
-print(min(len(name) for name in  stark_names))
+# print(min(len(name) for name in  stark_names))
 
 # prrint length of each name
-print([len(name) for name in stark_names])
+# print([len(name) for name in stark_names])
 
 # print longest name
-print(max(stark_names, key=lambda name:len(name)))
+# print(max(stark_names, key=lambda name:len(name)))
 
 songs = [
   {"title": "happy birthday", "playcount": 1},
@@ -90,7 +90,49 @@ songs = [
 ]
 
 # find song with least number of plays
-print(min(songs, key=lambda song:song['playcount'])['title'])
+# print(min(songs, key=lambda song:song['playcount'])['title'])
 
 ## reversed
-print(''.join(list(reversed('hello'))))
+# print(''.join(list(reversed('hello'))))
+
+def max_magnitude(nums):
+    return max(abs(num) for num in nums)
+
+# print(max_magnitude([200, 300,-900]))
+
+## zip
+first_zip = zip([1,2,3], [4,5,6])
+
+# print(list(first_zip))
+# print(dict(first_zip))
+
+# get higher score for each student
+midterms = [80,91,78]
+finals = [98,89,57]
+students = ['joel', 'ellie', 'tommy']
+
+final_grades =  [max(pair) for pair in zip(midterms, finals)]
+
+final_scores = dict(zip(students, final_grades))
+# print(final_scores)
+
+# interleave('aaa', 'zzz') => 'azazaz'
+def interleave(str1, str2):
+  # str_tups = list(zip(list(str1), list(str2)))
+  # return ''.join([''.join(item) for item in str_tups])
+
+  # or
+  return ''.join(''.join(x) for x in (zip(str1,str2)))
+
+# print(interleave('lzr', 'iad'))
+
+def triple_and_filter(nums):
+  return list(map(lambda num: num * 3, list(filter(lambda num: num % 4 == 0, nums))))
+
+
+# print(triple_and_filter([1,2,3,4]))
+
+def extract_full_name(names):
+  return [name['first'] + ' ' + name['last'] for name in names]
+
+print(extract_full_name([{'first': 'Elie', 'last': 'Schoppik'}, {'first': 'Colt', 'last': 'Steele'}]))
